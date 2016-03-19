02aac295590af381e27db74f122bf2bd6f95b790
James Hiebert <hiebert@uvic.ca>
2016-03-19 00:37:20 -0700

    Rewrite of the main stage 4 loop (WIP)

--- R/QPQM/stage.4.qpqm.r
+++ R/QPQM/stage.4.qpqm.r
@@ -19,16 +19,16 @@ qdm.netcdf.wrapper <- function(qpqm.file, bcca.file, analogues, out.file, varnam
     ## All files should have the same spatial domain.\n\n')
     ptm <- proc.time()
 
-    qpqm <- nc_open(qpqm.file)
-    bcca <- nc_open(bcca.file)
-    #out  <- nc_open(out.file,write=TRUE)
+    qpqm.nc <- nc_open(qpqm.file)
+    bcca.nc <- nc_open(bcca.file)
+    #out.nc  <- nc_open(out.file,write=TRUE)
 
-    nlat <- qpqm$dim$lat$len
-    nlon <- qpqm$dim$lon$len
+    nlat <- qpqm.nc$dim$lat$len
+    nlon <- qpqm.nc$dim$lon$len
 
-    bcca.time <- compute.time.stats(nc)
+    bcca.time <- compute.time.stats(bcca.nc)
 
-    n.qpqm <- qpqm$dim$time$len
+    n.qpqm <- qpqm.nc$dim$time$len
     nt <- bcca.time$n
 
     ncells <- nlat * nlon
@@ -50,51 +50,89 @@ qdm.netcdf.wrapper <- function(qpqm.file, bcca.file, analogues, out.file, varnam
         print(paste("Processing steps", i_0, "-", i_n, "/", nt))
 
         # Read all space for the time chunk
-        var.bcca <- ncvar_get(bcca,varid=varname,start=c(1,1,i_0),count=c(nlon,nlat,ni))
-        var.qpqm <- ncvar_get(qpqm,varid=varname,start=c(1,1,i_0),count=c(nlon,nlat,ni))
+        var.bcca <- ncvar_get(bcca.nc, varid=varname, start=c(1,1,i_0), count=c(nlon,nlat,ni))
+        var.qpqm <- ncvar_get(qpqm.nc, varid=varname, start=c(1,1,i_0), count=c(nlon,nlat,ni))
 
         date.sub <- bcca.time$vals[i_0:i_n]
         month.factor <- as.factor(format(date.sub, '%Y-%m'))
 
+        # Cell loop
         dqm <- foreach(
-            b.list=split(var.bcca, month.factor),
-            q.list=split(var.qpqm, month.factor),
-            .combine=abind,
-            .inorder=TRUE) %do% {
+            bcca=split(var.bcca, 1:ncells),
+            qpqm=split(var.qpqm, 1:ncells),
+            .multicombine=TRUE,
+            .errorhandling='pass', #FIXME
+            .final=function(x) {
+                for (item in x) {
+                    if (! (is.numeric(item) || all(is.na(item)))) {
+                        browser()
+                    }
+                }
+                array(unlist(x, use.names=F), dim=c(nlat, nlon, ni))
+            },
+            .inorder=TRUE) %loop% {
+                if (all(is.na(bcca) && all(is.na(qpqm)))) {
+                    return(rep(NA, nt))
+                }
 
-                b.list <- jitter(b.list, 0.01)
-
-                print(dim(b.list))
-                slen <- dim(b.list)[3]
-                # FIXME: We need to actually apply the BCCA analogues to the obs here
-                # For each point in space, rank the days within a single month
-                bcca.ranks <- round(aperm(apply(b.list,c(1,2),rank,ties.method='average'),c(2,3,1))) ##Permute to fix the rank permute
-                data.p <- aperm(q.list,c(3,1,2))
-
-                # Permute the QPQM data into time x cell...
-                dim(data.p) <- c(slen,nlon*nlat)
-                # ... then convert it to a list
-                data.l <- lapply(apply(data.p,2,as.list),unlist)
-
-                # Permute the BCCA ranks as well into time x cell...
-                index.p <- aperm(bcca.ranks,c(3,1,2))
-                dim(index.p) <- c(slen,nlon*nlat)
-                # ... and make them a list
-                index.l <- lapply(apply(index.p,2,as.list),unlist)
-
-                # For each cell, reorder the time steps based on the ranks
-                result <- mapply(reorder,data.l,index.l)
-                dim(result) <- c(slen,nlon,nlat)
-                aperm(result,c(2,3,1))
+                # Jitter BCCA
+                bcca <- jitter(bcca, 0.01)
+                # Apply BCCA analogues
+                # Month loop
+                rv <- mapply(
+                    function(x, y) {
+                        if (all(is.na(x) && all(is.na(y)))) {
+                            return(rep(NA, nt))
+                        }
+                        # Rank the days with bcca
+                        ranks <- rank(x, ties.method='average')
+                        # Reorder the days of qpqm
+                        reorder(y, ranks)
+                       },
+                    x = split(bcca, month.factor),
+                    y = split(qpqm, month.factor)
+                    )
+                unsplit(rv, month.factor)
             }
+        ## dqm <- foreach(
+        ##     b.list=split(var.bcca, month.factor),
+        ##     q.list=split(var.qpqm, month.factor),
+        ##     .combine=abind,
+        ##     .inorder=TRUE) %do% {
 
-        ncvar_put(nc=out, varid=varname, vals=dqm,
-                  start=c(1, 1, n_0), count=c(-1, -1, ni))
+        ##         b.list <- jitter(b.list, 0.01)
+
+        ##         print(dim(b.list))
+        ##         slen <- dim(b.list)[3]
+        ##         # FIXME: We need to actually apply the BCCA analogues to the obs here
+        ##         # For each point in space, rank the days within a single month
+        ##         bcca.ranks <- round(aperm(apply(b.list,c(1,2),rank,ties.method='average'),c(2,3,1))) ##Permute to fix the rank permute
+        ##         data.p <- aperm(q.list,c(3,1,2))
+
+        ##         # Permute the QPQM data into time x cell...
+        ##         dim(data.p) <- c(slen,nlon*nlat)
+        ##         # ... then convert it to a list
+        ##         data.l <- lapply(apply(data.p,2,as.list),unlist)
+
+        ##         # Permute the BCCA ranks as well into time x cell...
+        ##         index.p <- aperm(bcca.ranks,c(3,1,2))
+        ##         dim(index.p) <- c(slen,nlon*nlat)
+        ##         # ... and make them a list
+        ##         index.l <- lapply(apply(index.p,2,as.list),unlist)
+
+        ##         # For each cell, reorder the time steps based on the ranks
+        ##         result <- mapply(reorder,data.l,index.l)
+        ##         dim(result) <- c(slen,nlon,nlat)
+        ##         aperm(result,c(2,3,1))
+        ##     }
+
+        #ncvar_put(nc=out.nc, varid=varname, vals=dqm,
+        #          start=c(1, 1, n_0), count=c(-1, -1, ni))
     }
-
-    nc_close(qpqm)
-    nc_close(bcca)
-    nc_close(out)
+    browser()
+    nc_close(qpqm.nc)
+    nc_close(bcca.nc)
+    #nc_close(out)
 
     print('Elapsed time')
     print(proc.time() - ptm)
