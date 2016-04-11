2e71d407e25a6fa0899833717308e3350faacfcb
James Hiebert <james@hiebert.name>
2016-04-11 11:42:50 -0700

    Adds wrapper script for QDM

---
+++ exec/QDM.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(TRUE)
+
+## Help section
+usage <- function() {
+    cat('
+      Quantile Delta Mapping???
+
+      Usage:
+      ./QDM.R [qpqm_file] [bcci_file] [analogues_file] [output_file] [variable_name]
+
+      Arguments:
+      qpqm_file - The output file from the QPQM script
+      bcci_file - The output file from the BCCI script
+      analogues_file - The output file from the BCCA script
+      output_file - The file to create (or overwrite) with the ...
+      varname - The name of the variable to downscale
+
+      All files (save for the analogues_file) should have the same spatial domain.\n\n'
+    )
+
+    q(save='no')
+}
+
+args <- as.list(commandArgs(trailingOnly=TRUE))
+if (length(args) != 5) {
+    usage()
+    quit(status=1)
+}
+
+names(args) <- c('qpqm.file', 'bcci.file', 'analogues.file', 'output.file', 'varid')
+attach(args)
+
+load(analogues.file)
+qdm.netcdf.wrapper(gcm.file, obs.file, analogues, output.file, varid)
