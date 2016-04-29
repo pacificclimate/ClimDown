e9513a6d868b31c304aef21419f1fe1e5005c59a
James Hiebert <james@hiebert.name>
2016-04-29 09:56:31 -0700

    Fixes formatting and applies logic optimizations
    in netcdf.calendar.R

--- R/netcdf.calendar.R
+++ R/netcdf.calendar.R
@@ -1,13 +1,7 @@
-##******************************************************************************
-# Convert NetCDF time dimension to a YMDH calendar
-# Alex Cannon (acannon@uvic.ca)
-##******************************************************************************
-
-#library(RNetCDF)
+# Convert NetCDF time dimension to a PCICt vector
 
 netcdf.calendar <-
-function(nc, time.variable='time')
-{
+function(nc, time.variable='time') {
     time.calendar <- ncatt_get(nc, time.variable, 'calendar')$value
     time.units <- ncatt_get(nc, time.variable, 'units')$value
     time.values <- ncvar_get(nc, time.variable)
@@ -15,11 +9,17 @@ function(nc, time.variable='time')
     origin.pcict <- as.PCICt(strsplit(time.units, ' ')[[1]][3],
                              cal=time.calendar)
 
-    if(time.calendar=='noleap') time.calendar <- '365_day'
-    if(time.calendar==0) time.calendar <- 'gregorian'
-    if(time.calendar=='standard') time.calendar <- 'gregorian'
-    if(grepl('days', time.units)) time.values <- time.values*86400
-    if(grepl('hours', time.units)) time.values <- time.values*3600
+    if (time.calendar == 'noleap') {
+        time.calendar <- '365_day'
+    }
+    else if (time.calendar == 0 || time.calendar == 'standard') {
+        time.calendar <- 'gregorian'
+    }
+    if (grepl('days', time.units)) {
+        time.values <- time.values * 86400
+    } else if(grepl('hours', time.units)) {
+        time.values <- time.values * 3600
+    }
     origin.pcict + time.values
 }
 
