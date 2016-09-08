3ec0caf85ebdd597f316b8cdf5da76f05075cb93
James Hiebert <hiebert@uvic.ca>
2016-09-08 13:26:51 -0700

    Documents wrapper, global and parallel options

--- R/BCCAQ.R
+++ R/BCCAQ.R
@@ -1,6 +1,29 @@
 #' @title Wrapper function for the entire BCCAQ downscaling method
 #'
-#' @description This function does everything
+#' @description BCCAQ is a hybrid downscaling method that combines
+#' outputs from Climate Analogues (CA) and quantile mapping at the
+#' fine-scale resolution.  First, the CA and climate imprint (CI)
+#' plus quantile delta mapping (QDM) algorithms are run
+#' independently. BCCAQ then combines outputs from the two by
+#' taking the daily QDM outputs at each fine-scale grid point and
+#' reordering them within a given month according to the daily
+#' CA ranks, i.e., using a form of Empirical Copula
+#' Coupling.
+#'
+#' The combination mitigates some potential issues with
+#' the separate algorithms. First, because the optimal weights
+#' used to combine the analogues in BCCA are derived on a
+#' day-by-day basis, without reference to the full historical data
+#' set, the algorithm may fail to reproduce long-term trends from
+#' the climate model. Second, the CI/QDM bias correction step
+#' fixes precipitation "drizzle" and other residual biases caused
+#' by the linear combination of daily fields from CA. Third,
+#' reordering data for each fine-scale grid point within a month
+#' effectively breaks the overly smooth representation of sub
+#' grid-scale spatial variability inherited from CI/QDM, thereby
+#' resulting in a more accurate representation of event-scale
+#' spatial gradients; this also prevents the downscaled outputs
+#' from drifting too far from the climate model's long-term trend.
 #'
 #' @param gcm.file Filename of GCM simulations in NetCDF format
 #' @param obs.file Filename of high-res gridded historical observations
